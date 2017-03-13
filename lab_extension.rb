f3a12dd30bb8562b655d8fbe235748f8cfb32ed9
Dominic <mrdjfraser@gmail.com>
2017-03-13 17:06:50 +0000

    test_title_returns_rental_details passing

--- lab_extension.rb
+++ lab_extension.rb
@@ -6,8 +6,41 @@ class Library
     @books_array = books_array
   end
 
+  def book_info(title)
+    for book in @books_array
+      if book[:title] == title
+      return book
+      end
+    end 
+  end
 
+def rental_info(title)
+  @books_array.each do |book| 
+    if book[:title] == title
+      return book[:rental_details]
+    end
+  end
 end
 
 
 
+
+end
+
+# Create a method that takes in a book title and returns only the rental details for that book.
+
+# [{title: "book1", rental_details: {student_name: "Jake", date: "01/01/2010"}}, {
+#       title: "book2",
+#       rental_details: {
+#         student_name: "Sam",
+#         date: "01/02/2010"
+#       }
+#     },
+#     {
+#       title: "book3",
+#       rental_details: {
+#         student_name: "Rob",
+#         date: "01/03/2010"
+#       }
+#     }
+#   ]
--- lab_extension.spec.rb
+++ lab_extension.spec.rb
@@ -35,5 +35,21 @@ def test_list_all_books
   assert_equal(@books, @library1.books_array)
 end
 
+def test_info_of_given_book
+  assert_equal({
+      title: "book1",
+      rental_details: {
+        student_name: "Jake",
+        date: "01/01/2010"
+        }
+      }, @library1.book_info("book1"))
+end
+
+def test_title_returns_rental_details
+  assert_equal({
+        student_name: "Jake",
+        date: "01/01/2010"
+        }, @library1.rental_info("book1"))
+end
 
 end

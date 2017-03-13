0190203f0d64fe926ecdea9bbfad3c586d4b2314
Dominic <mrdjfraser@gmail.com>
2017-03-13 14:52:01 +0000

    basics of Student class and TestStudent set up

---
+++ lab_part_a.rb
@@ -0,0 +1,10 @@
+class Student
+
+  def initialize(s_name, cohort)
+    @s_name = s_name
+    @cohort = cohort
+  end
+
+  
+
+end
--- lab_part_a_spec.rb
+++ lab_part_a_spec.rb
@@ -2,7 +2,11 @@ require ("minitest/autorun")
 require ("minitest/rg")
 require_relative ("lab_part_a")
 
-class TestBankAccount < MiniTest::Test
+class TestStudent < MiniTest::Test
+
+  def setup
+    @student_1 = Student.new("Jake", 12)
+  end
 
 
 end

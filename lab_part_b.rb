b92b506b2d3125a1a3784fcfd55aa47a355b3b38
Dominic <mrdjfraser@gmail.com>
2017-03-13 15:51:07 +0000

    added points instance variable

--- lab_part_b.rb
+++ lab_part_b.rb
@@ -2,10 +2,11 @@ class SportsTeam
   
   attr_accessor :team_name, :players, :coach
 
-  def initialize(team_name, players, coach)
+  def initialize(team_name, players, coach, points)
     @team_name = team_name
     @players = players
-    @coach = coach
+    @coach = 
+    @points = points
   end
 
   def team_name

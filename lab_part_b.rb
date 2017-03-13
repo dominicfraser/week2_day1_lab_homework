665c8885c7d76c5b7c906d0b30cdd3b1bf536177
Dominic <mrdjfraser@gmail.com>
2017-03-13 15:26:33 +0000

    test_get_team_name passing

--- lab_part_b.rb
+++ lab_part_b.rb
@@ -1,8 +1,15 @@
 class SportsTeam
+  
+  # attr_accessor :team_name, :players, :coach
+
   def initialize(team_name, players, coach)
     @team_name = team_name
     @players = players
     @coach = coach
+  end
+
+  def team_name
+    return @team_name
   end
 
 end
--- lab_part_b_spec.rb
+++ lab_part_b_spec.rb
@@ -2,9 +2,13 @@ require ("minitest/autorun")
 require ("minitest/rg")
 require_relative ("lab_part_b")
 
-class SportsTeam < MiniTest::Test
+class TestSportsTeam < MiniTest::Test
   def setup
     @example_team = SportsTeam.new("Jakes",["Sam1","Sam2","Sam3"],"Bob")
+  end
+
+  def test_get_team_name
+    assert_equal("Jakes", @example_team.team_name)
   end
 
 end
